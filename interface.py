88d013d881bf1d6f9ac74f701489d69af29c421c
Samuel Ramos <104317445+Thesamramos@users.noreply.github.com>
2024-10-14 18:37:23 -0400

    importação tkinter arrumada e configuração da janela adicionada

--- interface.py
+++ interface.py
@@ -1,5 +1,11 @@
-import _tkinter as tk
+import tkinter as tk
 
 class App:
     def __init__(self, root):
         self.root = root
+        self.setWindowConfig()
+    def setWindowConfig(self):
+        self.root.title("TO-DO LIST")
+        self.root.geometry("700x500")
+        self.root.resizable(True, True)
+        self.root.minsize(width=500, height=400)
