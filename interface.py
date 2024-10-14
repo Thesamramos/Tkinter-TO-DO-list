b7c210ec3725cea3dfd956b8d89e049a47497593
Samuel Ramos <104317445+Thesamramos@users.noreply.github.com>
2024-10-14 18:05:28 -0400

    Configurações iniciais dos arquivos main e interface

---
+++ interface.py
@@ -0,0 +1,5 @@
+import _tkinter as tk
+
+class App:
+    def __init__(self, root):
+        self.root = root
---
+++ main.py
@@ -0,0 +1,7 @@
+from tkinter import Tk
+from interface import App
+
+if __name__ == "__main__":
+    root = Tk()
+    app = App(root)
+    root.mainloop()
